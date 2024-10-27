aaeb0f79da7e5f7e4e1698bdb5abc32a90ae8994
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-27 21:23:15 +0300

    betty the code use pycodesyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 Defines a function named `index_range`
 """
 
+
 def index_range(page, page_size):
     """Return a tuple of size two containing a start index and an end index"""
     start = (page - 1) * page_size
