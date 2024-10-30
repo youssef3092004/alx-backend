a3a8f7a1b62d1e8d8404a836878c2027cf3dc449
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-31 01:16:58 +0300

    Implement MRUCache class with Most Recently Used caching mechanism
    - Inherited from BaseCaching.
    - Added put method to store items in the cache and manage eviction of the most recently used item when exceeding MAX_ITEMS.
    - Implemented get method to retrieve items and update their usage order.
    - Included docstrings for class and methods to provide clear documentation of functionality.

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' MRU Cache '''
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    ''' MRU Cache class '''
+
+    def __init__(self):
+        ''' Constructor '''
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        ''' Add an item in the cache '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key in self.order:
+                self.order.remove(key)
+            self.order.append(key)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                least_recently_used = self.order.pop(-2)
+                print(f"DISCARD: {least_recently_used}")
+                del self.cache_data[least_recently_used]
+
+    def get(self, key):
+        ''' Get an item by key '''
+        if key is None or key not in self.cache_data:
+            return None
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
