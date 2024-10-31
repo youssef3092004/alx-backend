b9b9b931784eb62da29916a7330fbde90cb07f09
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-31 03:58:29 +0300

    Implement MRUCache class with Most Recently Used caching strategy
    - Inherit from BaseCaching class.
    - Add methods to put and get items in the cache.
    - Implement item eviction based on the MRU strategy when exceeding MAX_ITEMS.
    - Update order tracking to reflect most recent access.
    - Include necessary documentation for methods and class.

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-''' MRU Cache '''
+""" MRU Cache """
+
 from base_caching import BaseCaching
 
+
 class MRUCache(BaseCaching):
-    ''' MRU Cache class '''
+    """ MRU Cache class that inherits from BaseCaching. """
 
     def __init__(self):
-        ''' Constructor '''
+        """ Constructor for the MRUCache class. """
         super().__init__()
         self.order = []
 
     def put(self, key, item):
-        ''' Add an item in the cache '''
+        """ Add an item in the cache. """
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key in self.order:
                 self.order.remove(key)
             self.order.append(key)
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                most_recently_used = self.order.pop()  # Remove the last item
-                print(f"DISCARD: {most_recently_used}")
-                del self.cache_data[most_recently_used]
+                mru_key = self.order.pop()
+                print(f"DISCARD: {mru_key}")
+                del self.cache_data[mru_key]
 
     def get(self, key):
-        ''' Get an item by key '''
+        """ Get an item by key. """
         if key is None or key not in self.cache_data:
             return None
         self.order.remove(key)
         self.order.append(key)
+        
         return self.cache_data[key]
