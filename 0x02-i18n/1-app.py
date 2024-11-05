9b151c1e9542044c4249ca7dbdd7c8b0f1d3f218
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-05 21:03:00 +0200

    feat: configure Flask app with Babel for i18n support
    - Added `Config` class to set language and timezone options
    - Integrated Babel with Flask app for internationalization
    - Configured default locale as en and timezone as UTC
    - Updated index route to render `1-index.html` template;

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,21 +1,31 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
 from flask_babel import Babel
 
-class Config:
-    """
-    Config class for Babel
-    """
-    app = Flask(__name__)
-    babel = Babel(app)
-    app.config.from_object(Config)
 
-    LANGUAGES = ["en", "fr"]
+class Config(object):
+    """
+    Application configuration class
+    """
+    LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    @app.route('/', strict_slashes=False)
-    def index():
-        """
-        Renders a basic html template
-        """
-        return render_template('1-index.html')
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
